f9d96c1d4583f3a49a942e235010ea9179d90fdf
Anthony Iniguez <anthony@Anthonys-MacBook-Pro.local>
2023-03-26 22:20:31 -0700

    final

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require ('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+const sequelize = new Sequelize(process.env.PG_URI)
+try {
+sequelize.authenticate().then(() => {   
+    console.log(`Connected to the database, ${process.env.PG_URI}${process.env.PG_URI}`)
+}).catch(err => {
+    console.log(`Error connecting to the database: `, err)
+})
 
 // ROOT
 app.get('/', (req, res) => {
